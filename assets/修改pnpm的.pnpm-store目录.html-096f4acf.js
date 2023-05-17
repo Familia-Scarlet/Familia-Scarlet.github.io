3d6a72f272704cbaee3e8e1070d66b54e04d1a78
Familia-Scarlet <84607943+Familia-Scarlet@users.noreply.github.com>
2023-05-17 12:48:09 +0000

    Deploying to master from @ Familia-Scarlet/familia-blog@84b2ae4974d67330864f05f3bc498d5f7ccfd216 🚀

---
+++ assets/修改pnpm的.pnpm-store目录.html-096f4acf.js
@@ -0,0 +1,20 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as i,c as n,e as s}from"./app-4ae3ebf1.js";const r={},t=s(`<ul><li>查看.pnpm-store目录</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>pnpm store path
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li>修改.pnpm-store目录</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>pnpm config set store-dir &quot;D:\\Program_Files\\nodejs\\.pnpm-store&quot;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>示例：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>Administrator@Familia-Desktop MINGW64 ~
+$ pnpm store path
+C:\\Users\\Administrator\\AppData\\Local\\pnpm\\store\\v3
+
+Administrator@Familia-Desktop MINGW64 ~
+$ pnpm config set store-dir D:\\Program_Files\\nodejs\\.pnpm-store
+
+Administrator@Familia-Desktop MINGW64 ~
+$ pnpm store path
+C:\\Users\\Administrator\\D:Program_Filesnodejs.pnpm-store\\v3
+
+Administrator@Familia-Desktop MINGW64 ~
+$ pnpm config set store-dir &quot;D:\\Program_Files\\nodejs\\.pnpm-store&quot;
+
+Administrator@Familia-Desktop MINGW64 ~
+$ pnpm store path
+D:\\Program_Files\\nodejs\\.pnpm-store\\v3
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,6),a=[t];function d(l,o){return i(),n("div",null,a)}const c=e(r,[["render",d],["__file","修改pnpm的.pnpm-store目录.html.vue"]]);export{c as default};
