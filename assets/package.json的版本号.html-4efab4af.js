d2db0f6a6ea5495f4b40a39e1d8132b4f698a9e7
familior <84607943+familior@users.noreply.github.com>
2023-06-08 15:01:29 +0000

    Deploying to master from @ familior/familiar-blog@8c2ac8649d156579600259436872ccd132fd4583 🚀

---
+++ assets/package.json的版本号.html-4efab4af.js
@@ -0,0 +1,5 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as t,c as o,e as s}from"./app-4f0865ac.js";const u={},n=s(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>&quot;@vuepress/client&quot;: &quot;2.0.0-beta.62&quot;,
+&quot;vue&quot;: &quot;3.3.2&quot;,
+&quot;vuepress&quot;: &quot;2.0.0-beta.62&quot;,
+&quot;vuepress-theme-hope&quot;: &quot;2.0.0-beta.211&quot;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>1、^意思是将当前库的版本更新到第一个数字，例：&quot;^4.1.0&quot;是库会更新到4.X.X的最新版本，但不会更新到5.X.X版本。</p><p>2、~意思是更新到中间数字的版本，例：&quot;~4.7.4&quot;是库会更新到4.7.X的最新版本，但不会更新到4.8.X版本。</p><p>锁定版本：</p><p>去掉^或~ 写死版本号</p>`,5),a=[n];function i(c,r){return t(),o("div",null,a)}const p=e(u,[["render",i],["__file","package.json的版本号.html.vue"]]);export{p as default};
